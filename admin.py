01fa820bf77c9434ffc830b2ea04cb579c02839d
Pratik date <pratikdate100@gmail.com>
2021-12-28 20:07:36 +0530

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from app.models import post,Order,Product
+
+# Register your models here.
+admin.site.register(post)
+admin.site.register(Order)
+admin.site.register(Product)
